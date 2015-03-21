c8f056309576d40e93f68ab50d303160346da949
sfhoughton <shoughton@brandeis.edu>
2015-03-21 15:00:48 -0400

    Adding additional comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Function makeCacheMatrix creats a special matrix object that can
 ## cache its inverse
 ## makeCacheMatrix does four things - 
